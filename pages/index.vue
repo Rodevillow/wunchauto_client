f9c55c26099faa72938c3ec037742703d5c7c5be
Родион Максименко <rodionmaksimenko@maximenko_r_macbook.lan>
2021-03-24 16:01:25 +0200

    Nuxt js Wunschauto

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <HomeSection />
+    <CredentialsSection />
+    <BenefitsSection />
+  </div>
+</template>
+
+<script>
+import HomeSection from "@/components/sections/HomeSection";
+import CredentialsSection from "@/components/sections/CredentialsSection";
+import BenefitsSection from "@/components/sections/BenefitsSection";
+
+export default {
+  components: {
+    HomeSection,
+    CredentialsSection,
+    BenefitsSection
+  }
+};
+</script>
